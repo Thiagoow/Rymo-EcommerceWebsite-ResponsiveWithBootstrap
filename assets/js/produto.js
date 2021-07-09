0c284920fe5cb61ee178542637fe81092e74d594
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-07-09 18:45:39 -0300

    Não consegui fazer o preço ser * pela quantidade, mas já está exibindo na DOM o preço via var

--- assets/js/produto.js
+++ assets/js/produto.js
@@ -1,5 +1,16 @@
-let smallImg = document.getElementsByClassName("small-img");
+/* Mostra o valor da variável em reais pro usuário na DOM: */
+let value = 38.0;
+let price = value.toLocaleString("pt-br", {
+  style: "currency",
+  currency: "BRL"
+});
+/* --------Muda o preço do produto a partir da quantidade
+definida pelo input number na DOM: */
+let quantity = document.getElementById("productQuantity").value;
+console.log(quantity);
 
+/* --------Muda as imagens da galeria: */
+let smallImg = document.getElementsByClassName("small-img");
 function changeMainImg(source) {
   let mainImg = document.getElementById("mainImg");
   mainImg.src = source.src;
